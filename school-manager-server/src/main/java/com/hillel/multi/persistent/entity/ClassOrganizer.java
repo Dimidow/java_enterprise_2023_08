3842b6cc1902cded99b223ec21fc6ffe0207bec1
Oleksandr-Obodov <oleksandr.obodov@ardas.biz>
2023-09-09 21:50:57 +0300

    Added new entity, service and controller for it

---
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entity/ClassOrganizer.java
@@ -0,0 +1,21 @@
+package com.hillel.multi.persistent.entity;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ClassOrganizer {
+
+    @NotNull
+    private List<Student> students;
+}
