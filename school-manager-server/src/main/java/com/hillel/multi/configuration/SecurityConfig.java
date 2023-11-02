fc3368de46aec5ac157e5cb78b4c55bb1698bace
Oleksandr-Obodov <oleksandr.obodov@ardas.biz>
2023-11-02 21:10:11 +0200

    Spring Security draft

---
+++ school-manager-server/src/main/java/com/hillel/multi/configuration/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.hillel.multi.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests
+                                                             .requestMatchers("/organizer/").permitAll()
+                                                             .requestMatchers("/organizer/add/", "/organizer/remove/").hasRole("Admin")
+                                                             .anyRequest().authenticated()
+        ).formLogin((form) -> form
+                                  .loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+}
---
+++ school-manager-server/src/main/java/com/hillel/multi/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hillel.multi.service;
+
+import com.hillel.multi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                   .findByUsername(username)
+                   .orElseThrow(
+                       () -> new UsernameNotFoundException(format("User: %s, not found", username)));
+    }
+}
